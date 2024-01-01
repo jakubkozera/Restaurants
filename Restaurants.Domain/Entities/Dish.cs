e5dfb94f264467b956ca994028549bab5e088673
Jakub Kozera <kozerakuba@gmail.com>
2024-01-01 11:14:09 +0100

    3.5 Exercise - migration

--- Restaurants.Domain/Entities/Dish.cs
+++ Restaurants.Domain/Entities/Dish.cs
@@ -7,5 +7,7 @@ public class Dish
     public string Description { get; set; } = default!;
     public decimal Price { get; set; }
 
+    public int? KiloCalories { get; set; }
+
     public int RestaurantId { get; set; }
 }
---
+++ Restaurants.Infrastructure/Migrations/20240101092608_KiloCaloriesAddedToDish.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurants.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class KiloCaloriesAddedToDish : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "KiloCalories",
+                table: "Dishes",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KiloCalories",
+                table: "Dishes");
+        }
+    }
+}
