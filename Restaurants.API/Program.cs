b8cdf162d02a1f64761c0bd019815c929a70761f
Jakub Kozera <kozerakuba@gmail.com>
2024-01-19 09:52:57 +0100

    8.3 Swagger support of identity

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -6,13 +6,35 @@ using Serilog.Events;
 using Serilog.Formatting.Compact;
 using Restaurants.API.Middlewares;
 using Restaurants.Domain.Entities;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { 
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "bearerAuth"}
+            },
+            []
+        }
+
+    });
+});
+
+builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddScoped<ErrorHandlingMiddleware>();
 builder.Services.AddScoped<RequestTimeLoggingMiddleware>();
@@ -44,7 +66,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapIdentityApi<User>();
+app.MapGroup("api/identity").MapIdentityApi<User>();
 
 app.UseAuthorization();
 
