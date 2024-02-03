4b2c288bf0dd9bd29d1a9f05b2bad79c46c80823
Jakub Kozera <kozerakuba@gmail.com>
2024-02-03 08:36:27 +0100

    10.4 Pagination request validation

---
+++ Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQueryValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
+
+public class GetAllRestaurantsQueryValidator : AbstractValidator<GetAllRestaurantsQuery>
+{
+    private int[] allowPageSizes = [5, 10, 15, 30];
+
+    public GetAllRestaurantsQueryValidator()
+    {
+        RuleFor(r => r.PageNumber)
+            .GreaterThanOrEqualTo(1);
+
+        RuleFor(r => r.PageSize)
+            .Must(value => allowPageSizes.Contains(value))
+            .WithMessage($"Page size must be in [{string.Join(",", allowPageSizes)}]");
+    }
+}
