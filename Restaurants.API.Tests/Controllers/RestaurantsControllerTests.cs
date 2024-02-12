1f6346df38f93c0c77f14b7f59635247060537ec
Jakub Kozera <kozerakuba@gmail.com>
2024-02-12 12:34:03 +0100

    11.12 Testing endpoints with authorization

--- Restaurants.API.Tests/Controllers/RestaurantsControllerTests.cs
+++ Restaurants.API.Tests/Controllers/RestaurantsControllerTests.cs
@@ -1,5 +1,16 @@
 ﻿using FluentAssertions;
+using Microsoft.AspNetCore.Authorization.Policy;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Moq;
+using Restaurants.API.Tests;
+using Restaurants.Application.Restaurants.Dtos;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Repositories;
+using System.Net;
+using System.Net.Http.Json;
 using Xunit;
 
 namespace Restaurants.API.Controllers.Tests;
@@ -7,10 +18,67 @@ namespace Restaurants.API.Controllers.Tests;
 public class RestaurantsControllerTests : IClassFixture<WebApplicationFactory<Program>>
 {
     private readonly WebApplicationFactory<Program> _factory;
+    private readonly Mock<IRestaurantsRepository> _restaurantsRepositoryMock = new();
 
     public RestaurantsControllerTests(WebApplicationFactory<Program> factory)
     {
-        _factory = factory;
+        _factory = factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
+                services.Replace(ServiceDescriptor.Scoped(typeof(IRestaurantsRepository),
+                                            _ => _restaurantsRepositoryMock.Object));
+            });
+        }) ;
+    }
+
+
+    [Fact]
+    public async Task GetById_ForNonExistingId_ShouldReturn404NotFound()
+    {
+        // arrange
+
+        var id = 1123;
+
+        _restaurantsRepositoryMock.Setup(m => m.GetByIdAsync(id)).ReturnsAsync((Restaurant?)null);
+
+        var client = _factory.CreateClient();
+
+        // act
+        var response = await client.GetAsync($"/api/restaurants/{id}");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task GetById_ForExistingId_ShouldReturn200Ok()
+    {
+        // arrange
+
+        var id = 99;
+
+        var restaurant = new Restaurant()
+        {
+            Id = id,
+            Name = "Test",
+            Description = "Test description"
+        };
+
+        _restaurantsRepositoryMock.Setup(m => m.GetByIdAsync(id)).ReturnsAsync(restaurant);
+
+        var client = _factory.CreateClient();
+
+        // act
+        var response = await client.GetAsync($"/api/restaurants/{id}");
+        var restaurantDto = await response.Content.ReadFromJsonAsync<RestaurantDto>();
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        restaurantDto.Should().NotBeNull();
+        restaurantDto.Name.Should().Be("Test");
+        restaurantDto.Description.Should().Be("Test description");
     }
 
     [Fact]
---
+++ Restaurants.API.Tests/FakePolicyEvaluator.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace Restaurants.API.Tests;
+
+internal class FakePolicyEvaluator : IPolicyEvaluator
+{
+    public Task<AuthenticateResult> AuthenticateAsync(AuthorizationPolicy policy, HttpContext context)
+    {
+        var claimsPrincipal = new ClaimsPrincipal();
+
+        claimsPrincipal.AddIdentity(new ClaimsIdentity(
+            new[]
+            {
+                    new Claim(ClaimTypes.NameIdentifier, "1"),
+                    new Claim(ClaimTypes.Role, "Admin"),
+            }));
+
+        var ticket = new AuthenticationTicket(claimsPrincipal, "Test");
+        var result = AuthenticateResult.Success(ticket);
+        return Task.FromResult(result);
+    }
+
+    public Task<PolicyAuthorizationResult> AuthorizeAsync(AuthorizationPolicy policy, AuthenticateResult authenticationResult, HttpContext context, object? resource)
+    {
+        var result = PolicyAuthorizationResult.Success();
+        return Task.FromResult(result);
+    }
+}
