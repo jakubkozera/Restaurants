22c3cd3da80e1958a25473ad9b5a1d45862db89d
Jakub Kozera <kozerakuba@gmail.com>
2024-02-12 08:32:52 +0100

    11.11 Controller integration tests

---
+++ Restaurants.API.Tests/Controllers/RestaurantsControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Restaurants.API.Controllers.Tests;
+
+public class RestaurantsControllerTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public RestaurantsControllerTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task GetAll_ForValidRequest_Returns200Ok()
+    {
+        // arrange
+        var client = _factory.CreateClient();
+
+        // act
+        var result = await client.GetAsync("/api/restaurants?pageNumber=1&pageSize=10");
+
+        // assert
+
+        result.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+
+    }
+
+    [Fact]
+    public async Task GetAll_ForInvalidRequest_Returns400BadRequest()
+    {
+        // arrange
+        var client = _factory.CreateClient();
+
+        // act
+        var result = await client.GetAsync("/api/restaurants");
+
+        // assert
+
+        result.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+
+    }
+}
--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -46,3 +46,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public partial class Program { }
