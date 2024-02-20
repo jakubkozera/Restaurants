8fcfef99a402eb8385caba50d226c66c5af90d6b
Jakub Kozera <kozerakuba@gmail.com>
2024-02-20 10:31:23 +0100

    12.10 Working with blobs locally

--- src/Restaurants.API/Controllers/RestaurantsController.cs
+++ src/Restaurants.API/Controllers/RestaurantsController.cs
@@ -74,7 +74,7 @@ public class RestaurantsController(IMediator mediator) : ControllerBase
         var command = new UploadRestaurantLogoCommand()
         {
             RestaurantId = id,
-            FileName = file.FileName,
+            FileName = $"{id}-{file.FileName}",
             File = stream
         };
 
