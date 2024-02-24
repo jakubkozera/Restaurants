dd7074ef21e9a71c763fd8f5e59d787c8703acbf
Jakub Kozera <kozerakuba@gmail.com>
2024-02-24 10:47:10 +0100

    13.2 CI in Github actions - fix tests

--- tests/Restaurants.API.Tests/Controllers/RestaurantsControllerTests.cs
+++ tests/Restaurants.API.Tests/Controllers/RestaurantsControllerTests.cs
@@ -9,6 +9,7 @@ using Restaurants.API.Tests;
 using Restaurants.Application.Restaurants.Dtos;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
+using Restaurants.Infrastructure.Seeders;
 using System.Net;
 using System.Net.Http.Json;
 using Xunit;
@@ -19,6 +20,7 @@ public class RestaurantsControllerTests : IClassFixture<WebApplicationFactory<Pr
 {
     private readonly WebApplicationFactory<Program> _factory;
     private readonly Mock<IRestaurantsRepository> _restaurantsRepositoryMock = new();
+    private readonly Mock<IRestaurantSeeder> _restaurantsSeederMock = new();
 
     public RestaurantsControllerTests(WebApplicationFactory<Program> factory)
     {
@@ -29,6 +31,10 @@ public class RestaurantsControllerTests : IClassFixture<WebApplicationFactory<Pr
                 services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
                 services.Replace(ServiceDescriptor.Scoped(typeof(IRestaurantsRepository),
                                             _ => _restaurantsRepositoryMock.Object));
+
+
+                services.Replace(ServiceDescriptor.Scoped(typeof(IRestaurantSeeder),
+                                            _ => _restaurantsSeederMock.Object));
             });
         }) ;
     }
